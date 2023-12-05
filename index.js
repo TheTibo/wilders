1d24978fc54672cf1959d5f4a02559c73eb2b2e1
Thibault Zhou <thetibo1@gmail.com>
2023-12-05 10:33:42 +0100

    chalk

---
+++ index.js
@@ -0,0 +1,7 @@
+import chalk from 'chalk';
+console.log(chalk.blue('hello Axelle!'));
+console.log(chalk.yellow('hello Farah!'));
+console.log(chalk.red('hello Margot!'));
+console.log(chalk.cyan('hello Paul!'));
+console.log(chalk.white('hello Yohan!'));
+console.log(chalk.gray('hello Thibault!'));
